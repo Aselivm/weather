a6853fa43647fe22d1bb9cabcff9ec5fb095972d
Stepan Primshits <Stepan Primshits>
2024-01-05 05:48:00 +0300

    SessionRepository add

--- src/main/java/org/primshic/stepan/dao/SessionRepository.java
+++ src/main/java/org/primshic/stepan/dao/SessionRepository.java
@@ -3,7 +3,6 @@ package org.primshic.stepan.dao;
 import org.hibernate.SessionFactory;
 import org.primshic.stepan.model.Session;
 import org.primshic.stepan.model.User;
-import org.primshic.stepan.services.UserService;
 import org.primshic.stepan.util.HibernateUtil;
 
 import java.util.Optional;
@@ -11,13 +10,24 @@ import java.util.Optional;
 public class SessionRepository {
     private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
     public void save(User user) {
+        try(org.hibernate.Session session = sessionFactory.openSession()){
+            session.beginTransaction();
+            session.persist(user);
+            session.getTransaction().commit();
+        }
     }
 
     public Optional<Session> getByUserId(int id) {
-        return null;
+        try(org.hibernate.Session session = sessionFactory.openSession()){
+            return Optional.ofNullable(session.createQuery("FROM Session WHERE user.id = :userId", Session.class)
+                    .setParameter("userId", id)
+                    .uniqueResult());
+        }
     }
 
     public Optional<Session> getById(String uuid) {
-        return null;
+        try(org.hibernate.Session session = sessionFactory.openSession()){
+            return  Optional.of(session.get(Session.class,uuid));
+        }
     }
 }
