c5dd206a72b809b9c49a3b365ac3a5282740a9ef
Stepan Primshits <Stepan Primshits>
2024-01-06 21:30:09 +0300

    delete location add

--- src/main/java/org/primshic/stepan/dao/LocationRepository.java
+++ src/main/java/org/primshic/stepan/dao/LocationRepository.java
@@ -9,8 +9,10 @@ import org.primshic.stepan.util.HibernateUtil;
 import javax.persistence.Query;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class LocationRepository {
+    private Logger log = Logger.getLogger(LocationRepository.class.getName());
     private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
     public List<Location> getUserLocations(User user) {
@@ -32,13 +34,20 @@ public class LocationRepository {
 
     public void delete(int userId, BigDecimal lat, BigDecimal lon) {
         try(Session session = sessionFactory.openSession()) {
-            String jpql = "DELETE FROM Location l WHERE l.user.id = :userId AND l.lat = :lat AND l.lon = :lon";
+            session.beginTransaction();
+            String jpql = "DELETE FROM Location l WHERE l.user.id = :userId " +
+                    "AND ROUND(l.lat, 4) = ROUND(:lat, 4) " +
+                    "AND ROUND(l.lon, 4) = ROUND(:lon, 4)";
 
             Query query = session.createQuery(jpql);
+            log.info("user id: "+userId);
+            log.info("lat: "+lat);
+            log.info("lon: "+lon);
             query.setParameter("userId", userId);
             query.setParameter("lat", lat);
             query.setParameter("lon", lon);
             query.executeUpdate();
+            session.getTransaction().commit();
         }
     }
 }
