537fb77016fa253749031647c4947f369b4dcfe8
Stepan Primshits <Stepan Primshits>
2024-01-08 09:46:48 +0300

    add Test_01_HibernateConnection class

---
+++ src/test/java/org/primshic/stepan/test_cases/Test_01_HibernateConnection.java
@@ -0,0 +1,74 @@
+package org.primshic.stepan.test_cases;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.NativeQuery;
+import org.junit.jupiter.api.*;
+import org.primshic.stepan.repo_mock.util.TestHibernateUtil;
+
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class Test_01_HibernateConnection {
+    private static SessionFactory sessionFactory;
+
+    @BeforeAll
+    public static void init() {
+        sessionFactory = TestHibernateUtil.getSessionFactory();
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("SessionFactory не NULL")
+    public void test01_SessionFactoryNotNull() {
+        assertNotNull(sessionFactory);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Подключение к БД через SessionFactory")
+    public void test02_ConnectionToDatabase() {
+        try (Session session = sessionFactory.openSession()) {
+            assertTrue(session.isConnected());
+        }
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Таблица users создана")
+    public void test03_usersTableExist() {
+        try (Session session = sessionFactory.openSession()) {
+            String sql = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'USERS' AND TABLE_SCHEMA = 'PUBLIC'";
+            NativeQuery<?> query = session.createNativeQuery(sql);
+            BigInteger result = (BigInteger) query.uniqueResult();
+            assertEquals(1, result.intValue());
+        }
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Таблица sessions создана")
+    public void test04_sessionsTableExist() {
+        try (Session session = sessionFactory.openSession()) {
+            String sql = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'SESSIONS' AND TABLE_SCHEMA = 'PUBLIC'";
+            NativeQuery<?> query = session.createNativeQuery(sql);
+            BigInteger result = (BigInteger) query.uniqueResult();
+            assertEquals(1, result.intValue());
+        }
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Таблица locations создана")
+    public void test05_locationsTableExist() {
+        try (Session session = sessionFactory.openSession()) {
+            String sql = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'LOCATIONS' AND TABLE_SCHEMA = 'PUBLIC'";
+            NativeQuery<?> query = session.createNativeQuery(sql);
+            BigInteger result = (BigInteger) query.uniqueResult();
+            assertEquals(1, result.intValue());
+        }
+    }
+}
--- src/test/resources/sql_scripts/create_users_table.sql
+++ src/test/resources/sql_scripts/create_users_table.sql
@@ -1,5 +1,5 @@
 create table Users(
                       ID int auto_increment primary key ,
-                      Login VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL UNIQUE,
+                      Login VARCHAR(100)NOT NULL UNIQUE,
                       Password varchar(60) NOT NULL
 );
